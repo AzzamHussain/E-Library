7fa4dfcedc0aaae47d79d7fd1c15468632918a79
AzzamHussain <azzamhussain70@gmail.com>
2024-12-21 02:20:23 +0500

    home  page in progress

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:e_books_app/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -14,8 +16,34 @@ class Homepage extends StatelessWidget {
             child: Expanded(
                 child: Row(
               children: [
-                Column(
-                  children: [Text("HomePage")],
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            "assets/images/dashboard.png",
+                            width: 40,
+                            height: 40,
+                            fit: BoxFit.cover,
+                          ),
+                          Text("E-Book"),
+                          CircleAvatar(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.background,
+                            child: Text(
+                              "A",
+                              style: TextStyle(color: fontColor),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 )
               ],
             )),
